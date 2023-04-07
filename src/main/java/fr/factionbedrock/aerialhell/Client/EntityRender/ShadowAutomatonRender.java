859b3a2fd4c444b2d904e42c7c679ce0cc99d084
Nicolas Hochart <76011128+nicochart@users.noreply.github.com>
2023-04-07 17:24:13 +0200

    [Entities] Render : Shadow Automaton/Troll inverted version

--- src/main/java/fr/factionbedrock/aerialhell/Client/EntityRender/ShadowAutomatonRender.java
+++ src/main/java/fr/factionbedrock/aerialhell/Client/EntityRender/ShadowAutomatonRender.java
@@ -5,8 +5,10 @@ import fr.factionbedrock.aerialhell.AerialHell;
 import fr.factionbedrock.aerialhell.Client.EntityModels.EmptyModel;
 import fr.factionbedrock.aerialhell.Client.EntityRender.Layers.ShadowAutomatonShadowLayer;
 import fr.factionbedrock.aerialhell.Entity.Monster.AutomatonEntity;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -14,7 +16,8 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 @OnlyIn(Dist.CLIENT)
 public class ShadowAutomatonRender<T extends AutomatonEntity, M extends EmptyModel<T>> extends MobRenderer<T,M>
 {
-	private static final ResourceLocation TEXTURE = new ResourceLocation(AerialHell.MODID, "textures/entity/automaton/shadow_automaton.png");
+	private static final ResourceLocation TEXTURE_NORMAL = new ResourceLocation(AerialHell.MODID, "textures/entity/automaton/shadow_automaton.png");
+	private static final ResourceLocation TEXTURE_INVERT = new ResourceLocation(AerialHell.MODID, "textures/entity/automaton/shadow_automaton_invert.png");
 
 	public ShadowAutomatonRender(EntityRendererManager rendererManager)
 	{
@@ -27,5 +30,9 @@ public class ShadowAutomatonRender<T extends AutomatonEntity, M extends EmptyMod
 		float f = 0.9F; matrixStackIn.scale(f, f, f);
 	}
 	
-	@Override public ResourceLocation getEntityTexture(AutomatonEntity entity) {return TEXTURE;}
+	@Override public ResourceLocation getEntityTexture(AutomatonEntity entity)
+	{
+		if (Minecraft.getInstance().player.isPotionActive(Effects.NIGHT_VISION)) {return TEXTURE_INVERT;}
+		else {return TEXTURE_NORMAL;}
+	}
 }
--- src/main/java/fr/factionbedrock/aerialhell/Client/EntityRender/ShadowTrollRender.java
+++ src/main/java/fr/factionbedrock/aerialhell/Client/EntityRender/ShadowTrollRender.java
@@ -4,8 +4,10 @@ import fr.factionbedrock.aerialhell.AerialHell;
 import fr.factionbedrock.aerialhell.Client.EntityModels.ShadowTrollModel;
 import fr.factionbedrock.aerialhell.Client.EntityRender.Layers.ShadowTrollShadowLayer;
 import fr.factionbedrock.aerialhell.Entity.Monster.ShadowTrollEntity;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -13,17 +15,18 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 @OnlyIn(Dist.CLIENT)
 public class ShadowTrollRender extends MobRenderer<ShadowTrollEntity, ShadowTrollModel>
 {	
-	private static final ResourceLocation TEXTURE = new ResourceLocation(AerialHell.MODID, "textures/entity/shadow_troll/shadow_troll.png");
-	
+	private static final ResourceLocation TEXTURE_NORMAL = new ResourceLocation(AerialHell.MODID, "textures/entity/shadow_troll/shadow_troll.png");
+	private static final ResourceLocation TEXTURE_INVERT = new ResourceLocation(AerialHell.MODID, "textures/entity/shadow_troll/shadow_troll_invert.png");
+
 	public ShadowTrollRender(EntityRendererManager rendererManager)
 	{
 		super(rendererManager, new ShadowTrollModel(true), 0.3F);
 		this.addLayer(new ShadowTrollShadowLayer(this));
 	}
 	
-	@Override
-	public ResourceLocation getEntityTexture(ShadowTrollEntity entity)
+	@Override public ResourceLocation getEntityTexture(ShadowTrollEntity entity)
 	{
-		return TEXTURE;
+		if (Minecraft.getInstance().player.isPotionActive(Effects.NIGHT_VISION)) {return TEXTURE_INVERT;}
+		else {return TEXTURE_NORMAL;}
 	}
 }
