aa282b9a4ff4dd95b0e127a092bcb6e6dcf6eb0a
Nicolas Hochart <76011128+nicochart@users.noreply.github.com>
2023-05-28 18:33:21 +0200

    [Items] Improved Tools attributes code

--- src/main/java/fr/factionbedrock/aerialhell/Item/Tools/AerialHellSwordItem.java
+++ src/main/java/fr/factionbedrock/aerialhell/Item/Tools/AerialHellSwordItem.java
@@ -29,10 +29,9 @@ public class AerialHellSwordItem extends SwordItem
 	{
 		super(tier, attackDamageIn, attackSpeedIn, builderIn);
 		ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
-		builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Tool modifier", (double)attackDamageIn + tier.getAttackDamageBonus(), AttributeModifier.Operation.ADDITION));
-		builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Tool modifier", (double)attackSpeedIn, AttributeModifier.Operation.ADDITION));
-		builder.put(Attributes.MOVEMENT_SPEED, new AttributeModifier(BASE_MOVEMENT_SPEED_UUID, "Weapon modifier", (double)movementSpeedIn, AttributeModifier.Operation.MULTIPLY_TOTAL));
-		builder.put(Attributes.MAX_HEALTH, new AttributeModifier(BASE_MAX_HEALTH_UUID, "Weapon modifier", (double)maxHealthIn, AttributeModifier.Operation.ADDITION));
+		builder.putAll(this.defaultModifiers);
+		if (movementSpeedIn != 0) builder.put(Attributes.MOVEMENT_SPEED, new AttributeModifier(BASE_MOVEMENT_SPEED_UUID, "Weapon modifier", (double)movementSpeedIn, AttributeModifier.Operation.MULTIPLY_TOTAL));
+		if (maxHealthIn != 0) builder.put(Attributes.MAX_HEALTH, new AttributeModifier(BASE_MAX_HEALTH_UUID, "Weapon modifier", (double)maxHealthIn, AttributeModifier.Operation.ADDITION));
 		this.defaultModifiers = builder.build();
 	}
 
