412727f087a8bded6922e7597fc4b16f05d91f55
Nicolas Hochart <76011128+nicochart@users.noreply.github.com>
2023-07-31 01:09:15 +0200

    [Worldgen, Features] Fixes/changes

--- src/main/java/fr/factionbedrock/aerialhell/Event/Listeners/WorldgenEventListener.java
+++ src/main/java/fr/factionbedrock/aerialhell/Event/Listeners/WorldgenEventListener.java
@@ -1,23 +1,9 @@
 package fr.factionbedrock.aerialhell.Event.Listeners;
 
-import fr.factionbedrock.aerialhell.Registry.Worldgen.AerialHellPlacedFeatures;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.levelgen.GenerationStep;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class WorldgenEventListener
 {
-    // TODO : use forge biome modifiers - https://forge.gemwire.uk/wiki/Biome_Modifiers#Builtin_Biome_Modifier_Types
-    /*@SubscribeEvent(priority = EventPriority.HIGH)
-    public static void BiomeLoadingEvent(final BiomeLoadingEvent event)
-    {
-        if (event.getCategory() != Biome.BiomeCategory.THEEND && event.getCategory() != Biome.BiomeCategory.NETHER)
-        {
-            Object RegisterPlacedFeatures;
-            event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).add(AerialHellPlacedFeatures.STELLAR_PORTAL_FRAME_ORE.getHolder().get());
-        }
-    }*/
+    //1.18.2 and before : was used to place stellar portal frame ore in overworld. Now done in aerialhell data
 }
