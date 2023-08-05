c080fab26a67597dda319195865ba0eb9be5cc3e
Nicolas Hochart <76011128+nicochart@users.noreply.github.com>
2023-08-05 21:56:49 +0200

    [Effects] Reworked Head in the Clouds effect for 1.20.1

--- src/main/java/fr/factionbedrock/aerialhell/Effect/HeadInTheCloudsEffect.java
+++ src/main/java/fr/factionbedrock/aerialhell/Effect/HeadInTheCloudsEffect.java
@@ -3,23 +3,29 @@ package fr.factionbedrock.aerialhell.Effect;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.phys.Vec3;
 
 public class HeadInTheCloudsEffect extends MobEffect
 {
-    public HeadInTheCloudsEffect(MobEffectCategory typeIn, int liquidColorIn)
-    {
-        super(typeIn, liquidColorIn);
-    }
+    public HeadInTheCloudsEffect(MobEffectCategory typeIn, int liquidColorIn) {super(typeIn, liquidColorIn);}
 
     @Override
     public void applyEffectTick(LivingEntity entityLivingBaseIn, int amplifier)
     {
-    	//entityLivingBaseIn.flyingSpeed = 0.03f * (1 + amplifier); //TODO : flyingSpeed do not exist anymore. Find another solution
-		
-		if (entityLivingBaseIn.getDeltaMovement().y < -0.2 && !entityLivingBaseIn.isShiftKeyDown())
-		{
-			entityLivingBaseIn.setDeltaMovement(entityLivingBaseIn.getDeltaMovement().x, -0.2, entityLivingBaseIn.getDeltaMovement().z);
-		}
+        double x=entityLivingBaseIn.getDeltaMovement().x, y=entityLivingBaseIn.getDeltaMovement().y, z=entityLivingBaseIn.getDeltaMovement().z;
+        double xNew = x, yNew = y, zNew = z;
+        if (!entityLivingBaseIn.onGround())
+        {
+            double maxHorizontalSpeed = 0.17f * (1 + amplifier);
+            double horizontalSpeed = Math.sqrt(x * x + z * z);
+            if (horizontalSpeed < maxHorizontalSpeed) {xNew = x*1.1; zNew = z*1.1;} //maximize horizontal speed if speed is not already too high
+        }
+
+        double yMin = Math.min(-0.2 + 0.05 * amplifier, 0.0); //amplifier=0 : -0.2  ;  amplifier=1 : -0.15  ; amplifier=2 : -0.1  ;  amplifier=3 : -0.05  ;  amplifier=4+ : 0.0
+		if (entityLivingBaseIn.isCrouching()) {xNew /= 1.2; zNew /= 1.2; if (yNew > -2) {yNew -= 0.02;}} //slow down horizontal speed, faster vertical speed if player is crouching
+        else if (y < yMin) {yNew = yMin;} //minimize vertical speed if player is not crouching
+
+        entityLivingBaseIn.setDeltaMovement(xNew, yNew, zNew);
     }
 
     @Override
