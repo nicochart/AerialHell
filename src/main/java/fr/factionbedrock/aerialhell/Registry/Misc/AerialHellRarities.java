1436d6fcaa62d08045e2350cee86c65b0c3205ea
Nicolas Hochart <nicolas@freddom.fr>
2023-12-07 13:25:08 +0100

    [Rarities] +Rarities localizations, fixing issue #7

--- src/main/java/fr/factionbedrock/aerialhell/Registry/Misc/AerialHellRarities.java
+++ src/main/java/fr/factionbedrock/aerialhell/Registry/Misc/AerialHellRarities.java
@@ -5,9 +5,9 @@ import net.minecraft.world.item.Rarity;
 
 public class AerialHellRarities
 {
-	public static final Rarity FROZEN = Rarity.create("frozen", ChatFormatting.DARK_AQUA);
-	public static final Rarity CORRUPTED = Rarity.create("corrupted", ChatFormatting.DARK_PURPLE);
-	public static final Rarity VIBRANT = Rarity.create("vibrant", ChatFormatting.DARK_GREEN);
-	public static final Rarity LEGENDARY = Rarity.create("legendary", ChatFormatting.GOLD);
-	public static final Rarity MYTHICAL = Rarity.create("mythical", ChatFormatting.RED);
+	public static final Rarity FROZEN = Rarity.create("aerial_hell_frozen", ChatFormatting.DARK_AQUA);
+	public static final Rarity CORRUPTED = Rarity.create("aerial_hell_corrupted", ChatFormatting.DARK_PURPLE);
+	public static final Rarity VIBRANT = Rarity.create("aerial_hell_vibrant", ChatFormatting.DARK_GREEN);
+	public static final Rarity LEGENDARY = Rarity.create("aerial_hell_legendary", ChatFormatting.GOLD);
+	public static final Rarity MYTHICAL = Rarity.create("aerial_hell_mythical", ChatFormatting.RED);
 }
