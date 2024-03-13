48f61cc5a8148d0600887e17e0557fb19e574d58
Nicolas Hochart <nicolas@freddom.fr>
2024-03-13 17:45:08 +0100

    [Entities] Fixed Stellar Chicken crashing servers
    finalizeSpawn() and tick() (client and server side) were using client side only BiomeColors class

--- src/main/java/fr/factionbedrock/aerialhell/Entity/Passive/StellarChickenEntity.java
+++ src/main/java/fr/factionbedrock/aerialhell/Entity/Passive/StellarChickenEntity.java
@@ -26,6 +26,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.*;
+import net.minecraft.world.level.biome.Biome;
 
 import javax.annotation.Nullable;
 
@@ -38,7 +39,7 @@ public class StellarChickenEntity extends Chicken
 
     @Override public SpawnGroupData finalizeSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType mobSpawnType, @Nullable SpawnGroupData spawnGroupData, @Nullable CompoundTag tag)
     {
-        this.setColor(BiomeColors.getAverageGrassColor(level, this.blockPosition()));
+        this.setColor(getBlockPositionTint());
         return super.finalizeSpawn(level, difficulty, mobSpawnType, spawnGroupData, tag);
     }
 
@@ -46,10 +47,15 @@ public class StellarChickenEntity extends Chicken
     {
         if (this.getColor() == 0)
         {
-            int color = BiomeColors.getAverageGrassColor(this.level(), this.blockPosition());
+            int color = getBlockPositionTint();
             this.setColor(color != 0 ? color : 1);
         }
         super.tick();
+    }
+
+    private int getBlockPositionTint()
+    {
+        return this.level().getBlockTint(this.blockPosition(), Biome::getGrassColor);
     }
 
     @Override protected void defineSynchedData()
