48acdf76281db9dcae24fee9435137f1e7701fdc
Nicolas Hochart <nicolas@freddom.fr>
2024-04-03 23:44:44 +0200

    [Entities] Changed Pirates & Ninja Pirates textures

--- src/main/java/fr/factionbedrock/aerialhell/Client/EntityRender/SlimePirateRender.java
+++ src/main/java/fr/factionbedrock/aerialhell/Client/EntityRender/SlimePirateRender.java
@@ -8,6 +8,7 @@ import fr.factionbedrock.aerialhell.Client.EntityModels.SlimePirateModel;
 import fr.factionbedrock.aerialhell.Entity.Monster.Pirate.AbstractSlimePirateEntity;
 import fr.factionbedrock.aerialhell.Entity.Monster.Pirate.GhostSlimeNinjaPirateEntity;
 import fr.factionbedrock.aerialhell.Entity.Monster.Pirate.GhostSlimePirateEntity;
+import fr.factionbedrock.aerialhell.Entity.Monster.Pirate.SlimeNinjaPirateEntity;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
@@ -15,14 +16,17 @@ import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 
 @OnlyIn(Dist.CLIENT)
 public class SlimePirateRender extends MobRenderer<AbstractSlimePirateEntity, SlimePirateModel<AbstractSlimePirateEntity>>
 {
-    private static final ResourceLocation TEXTURE = new ResourceLocation(AerialHell.MODID, "textures/entity/pirate/slime.png");
-    private static final ResourceLocation TEXTURE_GHOST = new ResourceLocation(AerialHell.MODID, "textures/entity/pirate/ghost.png");
+    private static final ResourceLocation SLIME = new ResourceLocation(AerialHell.MODID, "textures/entity/pirate/slime.png");
+    private static final ResourceLocation SLIME_NINJA = new ResourceLocation(AerialHell.MODID, "textures/entity/pirate/slime_ninja.png");
+    private static final ResourceLocation GHOST = new ResourceLocation(AerialHell.MODID, "textures/entity/pirate/ghost.png");
+    private static final ResourceLocation GHOST_NINJA = new ResourceLocation(AerialHell.MODID, "textures/entity/pirate/ghost_ninja.png");
 
     public SlimePirateRender(EntityRendererProvider.Context context)
     {
@@ -41,5 +45,11 @@ public class SlimePirateRender extends MobRenderer<AbstractSlimePirateEntity, Sl
         matrixStackIn.scale(scale, scale, scale);
     }
 
-    @Override public ResourceLocation getTextureLocation(AbstractSlimePirateEntity entity) {return (entity instanceof GhostSlimePirateEntity || entity instanceof GhostSlimeNinjaPirateEntity) ? TEXTURE_GHOST : TEXTURE;}
+    @Override @NotNull public ResourceLocation getTextureLocation(AbstractSlimePirateEntity entity)
+    {
+        if (entity instanceof GhostSlimeNinjaPirateEntity) {return GHOST_NINJA;}
+        else if (entity instanceof SlimeNinjaPirateEntity) {return SLIME_NINJA;}
+        else if (entity instanceof GhostSlimePirateEntity) {return GHOST;}
+        else {return SLIME;}
+    }
 }
