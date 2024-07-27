751f1878ba0655888591ab080591411494f36825
Nicolas Hochart <76011128+nicochart@users.noreply.github.com>
2024-07-28 00:14:41 +0200

    [Entities] Fixed Lunar Priest phase 1 movements

--- src/main/java/fr/factionbedrock/aerialhell/Entity/Bosses/LunaticPriestEntity.java
+++ src/main/java/fr/factionbedrock/aerialhell/Entity/Bosses/LunaticPriestEntity.java
@@ -186,36 +186,34 @@ public class LunaticPriestEntity extends AbstractBossEntity
 		{
 			if (isActive())
 			{
-				if (this.isInWater())
-			    {
-			    	this.moveRelative(0.02F, travelVector);
-			        this.move(MoverType.SELF, this.getDeltaMovement());
-			        this.setDeltaMovement(this.getDeltaMovement().scale((double)0.8F));
-			    }
-				else if (this.isInLava())
+				if (this.isControlledByLocalInstance())
 				{
-			        this.moveRelative(0.02F, travelVector);
-			        this.move(MoverType.SELF, this.getDeltaMovement());
-			        this.setDeltaMovement(this.getDeltaMovement().scale(0.5D));
-			    }
-				else
-				{
-			        BlockPos ground = new BlockPos(this.getBlockX(), this.getBlockY() - 1, this.getBlockZ());
-			        float f = 0.91F;
-			        if (this.onGround())
-			        {
-			        	f = this.level().getBlockState(ground).getFriction(this.level(), ground, this) * 0.91F;
-			        }
-			        float f1 = 0.16277137F / (f * f * f);
-			        f = 0.91F;
-			        if (this.onGround())
-			        {
-			            f = this.level().getBlockState(ground).getFriction(this.level(), ground, this) * 0.91F;
-			        }
-		
-			        this.moveRelative(this.onGround() ? 0.1F * f1 : 0.02F, travelVector);
-			        this.move(MoverType.SELF, this.getDeltaMovement());
-			        this.setDeltaMovement(this.getDeltaMovement().scale((double)f));
+					if (this.isInWater())
+					{
+						this.moveRelative(0.02F, travelVector);
+						this.move(MoverType.SELF, this.getDeltaMovement());
+						this.setDeltaMovement(this.getDeltaMovement().scale(0.8F));
+					}
+					else if (this.isInLava())
+					{
+						this.moveRelative(0.02F, travelVector);
+						this.move(MoverType.SELF, this.getDeltaMovement());
+						this.setDeltaMovement(this.getDeltaMovement().scale(0.5));
+					}
+					else
+					{
+						BlockPos ground = getBlockPosBelowThatAffectsMyMovement();
+						float f = 0.91F;
+						if (this.onGround()) {f = this.level().getBlockState(ground).getFriction(this.level(), ground, this) * 0.91F;}
+
+						float f1 = 0.16277137F / (f * f * f);
+						f = 0.91F;
+						if (this.onGround()) {f = this.level().getBlockState(ground).getFriction(this.level(), ground, this) * 0.91F;}
+
+						this.moveRelative(this.onGround() ? 0.1F * f1 : 0.02F, travelVector);
+						this.move(MoverType.SELF, this.getDeltaMovement());
+						this.setDeltaMovement(this.getDeltaMovement().scale((double)f));
+					}
 				}
 				this.calculateEntityAnimation(false);
 			}
