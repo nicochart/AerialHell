f98dab5e51f15127c2cdd52677b4bd99abcf6a5d
Nicolas Hochart <nicolas@freddom.fr>
2024-09-08 23:34:19 +0200

    [Blocks, BlockEntities] Un-randomized blockState restore time
    TODO : fix blockentity data not (re)loaded after disconnect/reconnect

--- src/main/java/fr/factionbedrock/aerialhell/Block/CollisionCondition/IntangibleTemporaryBlock.java
+++ src/main/java/fr/factionbedrock/aerialhell/Block/CollisionCondition/IntangibleTemporaryBlock.java
@@ -2,15 +2,14 @@ package fr.factionbedrock.aerialhell.Block.CollisionCondition;
 
 import com.mojang.serialization.MapCodec;
 import fr.factionbedrock.aerialhell.BlockEntity.IntangibleTemporaryBlockEntity;
-import fr.factionbedrock.aerialhell.Registry.AerialHellBlocksAndItems;
+import fr.factionbedrock.aerialhell.Registry.AerialHellBlockEntities;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
@@ -19,39 +18,19 @@ import javax.annotation.Nullable;
 public class IntangibleTemporaryBlock extends CollisionConditionHalfTransparentBlockEntity
 {
     public static final MapCodec<IntangibleTemporaryBlock> CODEC = simpleCodec(IntangibleTemporaryBlock::new);
-    private final int lifetime = 1;
-    private int tickcount;
 
-    public IntangibleTemporaryBlock(Properties properties)
-    {
-        super(properties);
-        this.tickcount = 0;
-    }
+    public IntangibleTemporaryBlock(Properties properties) {super(properties);}
 
-    @Override protected MapCodec<? extends BaseEntityBlock> codec() {return CODEC;}
+    @Override protected MapCodec<? extends IntangibleTemporaryBlock> codec() {return CODEC;}
 
     @Nullable @Override public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {return new IntangibleTemporaryBlockEntity(pos, state);}
 
-    @Override protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource rand)
+    @Nullable @Override public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type)
     {
-        this.tickcount++;
-        if (this.tickcount >= this.lifetime) {level.setBlock(pos, this.getNextBlockState(level, pos), 2);}
+        return level.isClientSide ? null : createTickerHelper(type, AerialHellBlockEntities.INTANGIBLE_TEMPORARY_BLOCK.get(), IntangibleTemporaryBlockEntity::tick);
     }
 
     @Override protected boolean canEntityCollide(Entity entity) {return false;}
 
     @Override protected VoxelShape getCollidingShape() {return FULL_COLLISION_SHAPE;}
-
-    public void resetTickCount() {this.tickcount = 0;}
-
-    private BlockState getNextBlockState(LevelAccessor level, BlockPos pos)
-    {
-        BlockEntity blockentity = level.getBlockEntity(pos);
-        if (blockentity instanceof IntangibleTemporaryBlockEntity intangibleblockentity)
-        {
-            BlockState beforeState = intangibleblockentity.getBeforeState();
-            return beforeState == null || beforeState.getBlock() == AerialHellBlocksAndItems.INTANGIBLE_TEMPORARY_BLOCK.get() ? Blocks.AIR.defaultBlockState() : beforeState;
-        }
-        else {return Blocks.AIR.defaultBlockState();}
-    }
 }
--- src/main/java/fr/factionbedrock/aerialhell/BlockEntity/IntangibleTemporaryBlockEntity.java
+++ src/main/java/fr/factionbedrock/aerialhell/BlockEntity/IntangibleTemporaryBlockEntity.java
@@ -6,6 +6,8 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -14,10 +16,22 @@ import javax.annotation.Nullable;
 public class IntangibleTemporaryBlockEntity extends BlockEntity
 {
     private BlockState beforeState;
+    private int tickCount;
 
     public IntangibleTemporaryBlockEntity(BlockPos pos, BlockState blockState)
     {
         super(AerialHellBlockEntities.INTANGIBLE_TEMPORARY_BLOCK.get(), pos, blockState);
+        this.tickCount = 0;
+    }
+
+    public static void tick(Level level, BlockPos pos, BlockState state, IntangibleTemporaryBlockEntity blockEntity)
+    {
+        if (blockEntity.tickCount < 0) {blockEntity.tickCount = 0;}
+        blockEntity.tickCount++;
+        if (blockEntity.tickCount >= 180)
+        {
+            level.setBlock(pos, blockEntity.beforeState == null ? Blocks.AIR.defaultBlockState() : blockEntity.beforeState, 2);
+        }
     }
 
     public void setBeforeState(@Nullable BlockState state) {this.beforeState = state;}
@@ -26,19 +40,15 @@ public class IntangibleTemporaryBlockEntity extends BlockEntity
     @Override protected void saveAdditional(CompoundTag tag, HolderLookup.Provider registries)
     {
         super.saveAdditional(tag, registries);
-        if (beforeState != null)
-        {
-            tag.put("beforeState", NbtUtils.writeBlockState(beforeState));
-        }
+        tag.put("beforeState", NbtUtils.writeBlockState(beforeState == null ? Blocks.AIR.defaultBlockState() : beforeState));
+        tag.putInt("tickCount", tickCount);
     }
 
     @Override protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries)
     {
+        System.out.println("INTANTIBLE LOAD ADDITIONAL"); //not done. where is the block entity after disconnect/reconnect ?
         super.loadAdditional(tag, registries);
-        if (tag.contains("beforeState", 10))
-        {
-            beforeState = NbtUtils.readBlockState(this.level.holderLookup(Registries.BLOCK), tag.getCompound("beforeState"));
-            if (beforeState.isAir()) {beforeState = null;}
-        }
+        this.beforeState = NbtUtils.readBlockState(this.level.holderLookup(Registries.BLOCK), tag.getCompound("beforeState"));
+        this.tickCount = tag.getInt("tickCount");
     }
 }
