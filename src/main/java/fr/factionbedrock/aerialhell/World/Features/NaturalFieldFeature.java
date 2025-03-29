9a500a26fe87845fed9b9db291e8b896fff8a3df
Nicolas Hochart <nicolas@freddom.fr>
2025-03-29 18:59:10 +0100

    [Features] Generalization of Natural Field feature

--- src/main/java/fr/factionbedrock/aerialhell/World/Features/NaturalFieldFeature.java
+++ src/main/java/fr/factionbedrock/aerialhell/World/Features/NaturalFieldFeature.java
@@ -5,39 +5,41 @@ import fr.factionbedrock.aerialhell.Registry.AerialHellBlocks;
 import fr.factionbedrock.aerialhell.Registry.Misc.AerialHellTags;
 import fr.factionbedrock.aerialhell.Util.BlockHelper;
 import fr.factionbedrock.aerialhell.Util.FeatureHelper;
+import fr.factionbedrock.aerialhell.World.Features.Config.NaturalFieldConfig;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
-import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 
-public class StellarWheatFieldFeature extends Feature<NoneFeatureConfiguration>
+public class NaturalFieldFeature extends Feature<NaturalFieldConfig>
 {
     private static int MAX_VERTICAL_OFFSET = 3;
     private static float BASE_PROBABILITY_NOT_TO_PLACE_INDIVIDUAL_WHEAT = 0.1F;
     private static float BASE_PROBABILITY_TO_ACCEPT_OFFSET = 0.9F;
 
-    public StellarWheatFieldFeature(Codec<NoneFeatureConfiguration> codec) {super(codec);}
+    public NaturalFieldFeature(Codec<NaturalFieldConfig> codec) {super(codec);}
 
-    @Override public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> context)
+    @Override public boolean place(FeaturePlaceContext<NaturalFieldConfig> context)
     {
-        BlockPos blockPos = context.origin(); WorldGenLevel world = context.level(); RandomSource rand = context.random();
+        BlockPos blockPos = context.origin(); WorldGenLevel world = context.level();
 		boolean canGenerate = isAboveSurfaceBlockPos(world, blockPos) && !BlockHelper.hasAnySolidSurfaceAbove(world, blockPos.above(2), 3);
 		boolean generatesInDungeon = FeatureHelper.isFeatureGeneratingNextToDungeon(context);
 		
         if (canGenerate && !generatesInDungeon)
         {
-        	generateField(world, rand, blockPos);
+        	generateField(context);
         	return true;
         }
         return false;
     }
     
-    protected void generateField(WorldGenLevel world, RandomSource rand, BlockPos blockPos)
+    protected void generateField(FeaturePlaceContext<NaturalFieldConfig> context)
     {
+        WorldGenLevel world = context.level(); RandomSource rand = context.random(); BlockPos blockPos = context.origin();
     	BlockPos.MutableBlockPos placementPos = new BlockPos.MutableBlockPos();
     	boolean isBig = rand.nextDouble() > 0.8;
         int radiusX = getRandomRadius(rand, isBig);
@@ -48,26 +50,32 @@ public class StellarWheatFieldFeature extends Feature<NoneFeatureConfiguration>
             for(int z = -radiusZ; z <= radiusZ; z++)
             {
                 BlockPos pos = new BlockPos(x, 0, z);
-                if (isPosInsideEllipsis(pos, radiusX, radiusZ) && world.getRandom().nextFloat() > BASE_PROBABILITY_NOT_TO_PLACE_INDIVIDUAL_WHEAT)
+                if (isPosInsideEllipsis(pos, radiusX, radiusZ) && world.getRandom().nextFloat() < context.config().baseChance())
                 {
                     placementPos.set(blockPos.offset(pos));
-                    findHeightForPlacement(world, placementPos);
+                    findHeightForPlacement(context, placementPos);
                     if (isValidBlockPosForStellarWheat(world, placementPos))
                     {
                         world.setBlock(placementPos.below(), AerialHellBlocks.STELLAR_FARMLAND.get().defaultBlockState(), 2);
-                        world.setBlock(placementPos, AerialHellBlocks.STELLAR_WHEAT.get().defaultBlockState().setValue(CropBlock.AGE, world.getRandom().nextInt(4, 8)), 2);
+                        BlockState placementState = context.config().cropStateProvider().getState(rand, placementPos);
+                        if (placementState.getBlock() instanceof CropBlock)
+                        {
+                            placementState = placementState.setValue(CropBlock.AGE, world.getRandom().nextInt(4, 8));
+                        }
+                        world.setBlock(placementPos, placementState, 2);
                     }
                 }
             }
         }
     }
 
-    protected static void findHeightForPlacement(WorldGenLevel world, BlockPos.MutableBlockPos blockPos)
+    protected static void findHeightForPlacement(FeaturePlaceContext<NaturalFieldConfig> context, BlockPos.MutableBlockPos blockPos)
     {
+        WorldGenLevel world = context.level();
         int step = 0;
-        while (step < MAX_VERTICAL_OFFSET && !isAboveSurfaceBlockPos(world, blockPos))
+        while (step < context.config().maxVerticalOffset() && !isAboveSurfaceBlockPos(world, blockPos))
         {
-            float probabilityToMove = BASE_PROBABILITY_TO_ACCEPT_OFFSET / (step + 1);
+            float probabilityToMove = context.config().acceptOffsetChance() / (step + 1);
             if (world.getRandom().nextFloat() > probabilityToMove) {return;}
 
             if (world.getBlockState(blockPos).isAir()) {blockPos.move(Direction.DOWN);}
