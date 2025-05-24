f8d8c13e4b79ad15a086cde97e5feca93c970b55
Nicolas Hochart <nicolas@freddom.fr>
2025-05-24 18:22:12 +0200

    [Config] Versioning system with automatic backup & regen

--- src/main/java/fr/factionbedrock/aerialhell/Config/AerialHellConfig.java
+++ src/main/java/fr/factionbedrock/aerialhell/Config/AerialHellConfig.java
@@ -2,9 +2,20 @@ package fr.factionbedrock.aerialhell.Config;
 
 public class AerialHellConfig
 {
-    public float ShadowSpreadSpeedMultiplier = 1.0F;
-    public boolean enableShadowBindReloadTexture = true;
-    public boolean enableShadowBindTextureShift = true;
-    public boolean doBossGriefing = true;
-    public int overworldAbandonnedPortalSpacingOverride = 60;
+    public String configVersion;
+    public float shadowSpreadSpeedMultiplier;
+    public boolean enableShadowBindReloadTexture;
+    public boolean enableShadowBindTextureShift;
+    public boolean doBossGriefing;
+    public int overworldAbandonnedPortalSpacingOverride;
+
+    public AerialHellConfig()
+    {
+        this.configVersion = "none";
+        this.shadowSpreadSpeedMultiplier = 1.0F;
+        this.enableShadowBindReloadTexture = true;
+        this.enableShadowBindTextureShift = true;
+        this.doBossGriefing = true;
+        this.overworldAbandonnedPortalSpacingOverride = 60;
+    }
 }
--- src/main/java/fr/factionbedrock/aerialhell/Config/AerialHellConfigLoader.java
+++ src/main/java/fr/factionbedrock/aerialhell/Config/AerialHellConfigLoader.java
@@ -10,12 +10,16 @@ import java.io.Reader;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AerialHellConfigLoader
 {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     private static final Path CONFIG_FOLDER = FabricLoader.getInstance().getConfigDir().resolve(AerialHell.MODID);
     private static final Path CONFIG_PATH = CONFIG_FOLDER.resolve("config.json");
+    public static final String CONFIG_VERSION = "0.7.3.1";
 
     public static void loadAndStoreConfigParams()
     {
@@ -25,15 +29,38 @@ public class AerialHellConfigLoader
 
     public static AerialHellConfig loadConfig()
     {
+        AerialHellConfig config;
         if (!Files.exists(CONFIG_PATH))
         {
-            AerialHellConfig defaultConfig = new AerialHellConfig();
-            saveConfig(defaultConfig);
-            return defaultConfig;
+            config = new AerialHellDefaultConfig();
+            saveConfig(config);
+        }
+        else
+        {
+            try (Reader reader = Files.newBufferedReader(CONFIG_PATH)) {config = GSON.fromJson(reader, AerialHellConfig.class);}
+            catch (IOException e) {e.printStackTrace(); config = new AerialHellDefaultConfig();}
+
+            if (!config.configVersion.equals(CONFIG_VERSION))
+            {
+                AerialHell.LOGGER.warn("Aerial Hell : Outdated config -> backup created, new config file generated.");
+                backupOldConfig();
+                config = new AerialHellDefaultConfig();
+                saveConfig(config);
+            }
         }
 
-        try (Reader reader = Files.newBufferedReader(CONFIG_PATH)) {return GSON.fromJson(reader, AerialHellConfig.class);}
-        catch (IOException e) {e.printStackTrace(); return new AerialHellConfig();}
+        return config;
+    }
+
+    private static void backupOldConfig()
+    {
+        try
+        {
+            String timestamp = new SimpleDateFormat("yyyy-MM-dd-HHmmss").format(new Date());
+            Path backupPath = CONFIG_FOLDER.resolve("config-outdated_" + timestamp + ".json");
+            Files.move(CONFIG_PATH, backupPath, StandardCopyOption.REPLACE_EXISTING);
+        }
+        catch (IOException e) {e.printStackTrace();}
     }
 
     public static void saveConfig(AerialHellConfig config)
