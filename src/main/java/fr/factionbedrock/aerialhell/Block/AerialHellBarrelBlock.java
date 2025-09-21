72609f68ea4bfae9fdce1ba9ed8bac5f3f5ad978
Nicolas Hochart <nicolas@freddom.fr>
2025-09-21 23:49:39 +0200

    [Blocks] Removed useless TODO in AerialHellBarrelBlock

--- src/main/java/fr/factionbedrock/aerialhell/Block/AerialHellBarrelBlock.java
+++ src/main/java/fr/factionbedrock/aerialhell/Block/AerialHellBarrelBlock.java
@@ -3,15 +3,12 @@ package fr.factionbedrock.aerialhell.Block;
 import fr.factionbedrock.aerialhell.BlockEntity.AerialHellBarrelBlockEntity;
 
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.monster.piglin.PiglinAi;
 import net.minecraft.world.level.block.BarrelBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.InteractionResult;
@@ -20,8 +17,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.phys.BlockHitResult;
 
-import javax.annotation.Nullable;
-
 public class AerialHellBarrelBlock extends BarrelBlock
 {
 	/*
@@ -29,24 +24,13 @@ public class AerialHellBarrelBlock extends BarrelBlock
 	Because in BarrelBlockEntity, the barrel can close only if the barrel blockstate isIn(Blocks.BARREL).
 	*/
 	
-	public AerialHellBarrelBlock(BlockBehaviour.Properties properties)
-	{
-		super(properties);
-	}
+	public AerialHellBarrelBlock(BlockBehaviour.Properties properties) {super(properties);}
 
-	@Override
-	public BlockEntity newBlockEntity(BlockPos pos, BlockState state)
-	{
-		return new AerialHellBarrelBlockEntity(pos, state);
-	}
+	@Override public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {return new AerialHellBarrelBlockEntity(pos, state);}
 
-	@Override
-	public InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hit)
+	@Override public InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hit)
 	{
-		if (level.isClientSide())
-		{
-			return InteractionResult.SUCCESS;
-		}
+		if (level.isClientSide()) {return InteractionResult.SUCCESS;}
 		else
 		{
 			BlockEntity blockEntity = level.getBlockEntity(pos);
@@ -61,8 +45,7 @@ public class AerialHellBarrelBlock extends BarrelBlock
 		}
 	}
 
-	@Override
-	public void tick(BlockState state, ServerLevel world, BlockPos pos, RandomSource rand)
+	@Override public void tick(BlockState state, ServerLevel world, BlockPos pos, RandomSource rand)
 	{
 		BlockEntity blockEntity = world.getBlockEntity(pos);
 		if (blockEntity instanceof AerialHellBarrelBlockEntity)
@@ -71,22 +54,5 @@ public class AerialHellBarrelBlock extends BarrelBlock
 		}
 	}
 
-	@Override
-	public RenderShape getRenderShape(BlockState state)
-	{
-		return RenderShape.MODEL;
-	}
-
-	/*@Override TODO where is set the custom name ?
-	public void setPlacedBy(Level world, BlockPos pos, BlockState state, @Nullable LivingEntity placer,	ItemStack stack)
-	{
-		if (stack.hasCustomHoverName())
-		{
-			BlockEntity blockEntity = world.getBlockEntity(pos);
-			if (blockEntity instanceof AerialHellBarrelBlockEntity)
-			{
-				((AerialHellBarrelBlockEntity) blockEntity).setCustomName(stack.getHoverName());
-			}
-		}
-	}*/
+	@Override public RenderShape getRenderShape(BlockState state) {return RenderShape.MODEL;}
 }
