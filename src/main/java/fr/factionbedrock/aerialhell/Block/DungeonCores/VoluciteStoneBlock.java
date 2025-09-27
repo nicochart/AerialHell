a0562ae2fa9941cf58cc1111763fe43fdbfea87d
Nicolas Hochart <76011128+nicochart@users.noreply.github.com>
2025-09-27 13:46:13 +0200

    [Blocks] Animating random Volucite Stone blocks

---
+++ src/main/java/fr/factionbedrock/aerialhell/Block/DungeonCores/VoluciteStoneBlock.java
@@ -0,0 +1,46 @@
+package fr.factionbedrock.aerialhell.Block.DungeonCores;
+
+import fr.factionbedrock.aerialhell.Registry.AerialHellStateProperties;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+
+public class VoluciteStoneBlock extends CoreProtectedBlock
+{
+    public VoluciteStoneBlock(Properties properties)
+    {
+        super(properties);
+        this.registerDefaultState(this.defaultBlockState().setValue(CORE_PROTECTED, false).setValue(AerialHellStateProperties.SELF_LUMINESCENT, false));
+    }
+
+    @Override protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
+    {
+        super.createBlockStateDefinition(builder);
+        builder.add(AerialHellStateProperties.SELF_LUMINESCENT);
+    }
+
+    @Override protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random)
+    {
+        boolean wasCoreProtected = state.getValue(CORE_PROTECTED);
+        if (state.is(this) && random.nextFloat() > 0.9F && level.isLoaded(pos))
+        {
+            level.setBlockAndUpdate(pos, state.setValue(AerialHellStateProperties.SELF_LUMINESCENT, true).setValue(CORE_PROTECTED, wasCoreProtected));
+
+            level.scheduleTick(pos, this, 1060);
+        }
+    }
+
+    @Override protected boolean isRandomlyTicking(BlockState state)
+    {
+        return !state.getValue(AerialHellStateProperties.SELF_LUMINESCENT);
+    }
+
+    @Override protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random)
+    {
+        boolean wasCoreProtected = state.getValue(CORE_PROTECTED);
+        level.setBlockAndUpdate(pos, state.setValue(AerialHellStateProperties.SELF_LUMINESCENT, false).setValue(CORE_PROTECTED, wasCoreProtected));
+    }
+}
--- src/main/java/fr/factionbedrock/aerialhell/Registry/AerialHellStateProperties.java
+++ src/main/java/fr/factionbedrock/aerialhell/Registry/AerialHellStateProperties.java
@@ -5,4 +5,5 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 public class AerialHellStateProperties
 {
     public static final BooleanProperty SHIFTED_RENDER = BooleanProperty.create("shifted_render");
+    public static final BooleanProperty SELF_LUMINESCENT = BooleanProperty.create("self_luminescent");
 }
