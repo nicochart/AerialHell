6b9c23295168e97ae139c33d46f695feb5fe33dc
Nicolas Hochart <76011128+nicochart@users.noreply.github.com>
2025-10-20 22:43:38 +0200

    [Enchantments, LootTables] Added Vulnerability Aspect enchantment

--- src/main/java/fr/factionbedrock/aerialhell/Registry/AerialHellEnchantments.java
+++ src/main/java/fr/factionbedrock/aerialhell/Registry/AerialHellEnchantments.java
@@ -9,6 +9,7 @@ import net.minecraft.world.item.enchantment.Enchantment;
 public class AerialHellEnchantments
 {
     public static final ResourceKey<Enchantment> SOLID_ETHER_WALKER = key("solid_ether_walker");
+    public static final ResourceKey<Enchantment> VULNERABILITY_ASPECT = key("vulnerability_aspect");
 
     private static ResourceKey<Enchantment> key(String name)
     {
