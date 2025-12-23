bbfe538af08ce2761e30af7033545f3506e416c8
Nicolas Hochart <76011128+nicochart@users.noreply.github.com>
2025-12-23 18:13:13 +0100

    [Integration] Temporarily removed REI integration
    waiting for stable build for NeoForge 1.21.10

--- src/main/java/fr/factionbedrock/aerialhell/Integration/REI/FreezingRecipeDisplay.java
+++ src/main/java/fr/factionbedrock/aerialhell/Integration/REI/FreezingRecipeDisplay.java
@@ -1,22 +1,22 @@
-package fr.factionbedrock.aerialhell.Integration.REI;
-
-import me.shedaniel.rei.api.common.category.CategoryIdentifier;
-import me.shedaniel.rei.api.common.display.Display;
-import me.shedaniel.rei.api.common.display.DisplaySerializer;
-import me.shedaniel.rei.api.common.entry.EntryIngredient;
-import me.shedaniel.rei.api.common.entry.EntryStack;
-
-import java.util.List;
-
-public class FreezingRecipeDisplay extends AerialHellRecipeDisplay
-{
-    public static DisplaySerializer<FreezingRecipeDisplay> SERIALIZER = serializer(FreezingRecipeDisplay::new);
-
-    public FreezingRecipeDisplay(EntryStack<?> in, EntryStack<?> out) {super(in, out);}
-
-    public FreezingRecipeDisplay(EntryIngredient in, EntryIngredient out) {super(List.of(in), List.of(out));}
-
-    @Override public CategoryIdentifier<?> getCategoryIdentifier() {return AerialHellRei.FREEZING;}
-
-    @Override public DisplaySerializer<? extends Display> getSerializer() {return SERIALIZER;}
-}
+//package fr.factionbedrock.aerialhell.Integration.REI;
+//
+//import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+//import me.shedaniel.rei.api.common.display.Display;
+//import me.shedaniel.rei.api.common.display.DisplaySerializer;
+//import me.shedaniel.rei.api.common.entry.EntryIngredient;
+//import me.shedaniel.rei.api.common.entry.EntryStack;
+//
+//import java.util.List;
+//
+//public class FreezingRecipeDisplay extends AerialHellRecipeDisplay
+//{
+//    public static DisplaySerializer<FreezingRecipeDisplay> SERIALIZER = serializer(FreezingRecipeDisplay::new);
+//
+//    public FreezingRecipeDisplay(EntryStack<?> in, EntryStack<?> out) {super(in, out);}
+//
+//    public FreezingRecipeDisplay(EntryIngredient in, EntryIngredient out) {super(List.of(in), List.of(out));}
+//
+//    @Override public CategoryIdentifier<?> getCategoryIdentifier() {return AerialHellRei.FREEZING;}
+//
+//    @Override public DisplaySerializer<? extends Display> getSerializer() {return SERIALIZER;}
+//}
