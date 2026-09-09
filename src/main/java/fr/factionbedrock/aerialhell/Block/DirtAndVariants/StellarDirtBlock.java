c3dd027a8538c38ebb787922328db9e3a8bbb2aa
Nicolas Hochart <nicolas@freddom.fr>
2026-09-09 02:45:59 +0200

    [Blocks, Entities, Lang, Enchantments] Backported news / fixes

--- src/main/java/fr/factionbedrock/aerialhell/Block/DirtAndVariants/StellarDirtBlock.java
+++ src/main/java/fr/factionbedrock/aerialhell/Block/DirtAndVariants/StellarDirtBlock.java
@@ -15,15 +15,23 @@ public class StellarDirtBlock extends Block
 		super(properties);
 	}
 
-	@Override @Nullable
-	public BlockState getToolModifiedState(BlockState state, UseOnContext context, ItemAbility itemAbility, boolean simulate)
+	@Override @Nullable public BlockState getToolModifiedState(BlockState state, UseOnContext context, ItemAbility itemAbility, boolean simulate)
 	{
 		if (!context.getItemInHand().canPerformAction(itemAbility)) {return null;}
-		boolean isStellarDirt =  state.getBlock() == AerialHellBlocks.STELLAR_DIRT.get(), isStellarCoarseDirt = state.getBlock() == AerialHellBlocks.STELLAR_COARSE_DIRT.get(), isChiseledStellarDirt = state.getBlock() == AerialHellBlocks.CHISELED_STELLAR_DIRT.get();
-		if (isStellarDirt || isStellarCoarseDirt || isChiseledStellarDirt)
+		if (ItemAbilities.HOE_TILL == itemAbility)
 		{
-			if (ItemAbilities.HOE_TILL == itemAbility) {return isStellarCoarseDirt ? AerialHellBlocks.STELLAR_DIRT.get().defaultBlockState() : AerialHellBlocks.STELLAR_FARMLAND.get().defaultBlockState();}
-			if (ItemAbilities.SHOVEL_FLATTEN == itemAbility) {return AerialHellBlocks.STELLAR_DIRT_PATH.get().defaultBlockState();}
+			if (state.is(AerialHellBlocks.STELLAR_DIRT.get()) || state.is(AerialHellBlocks.CHISELED_STELLAR_DIRT.get()))
+			{
+				return AerialHellBlocks.STELLAR_FARMLAND.get().defaultBlockState();
+			}
+			else if (state.is(AerialHellBlocks.STELLAR_COARSE_DIRT.get()))
+			{
+				return AerialHellBlocks.STELLAR_DIRT.get().defaultBlockState();
+			}
+		}
+		else if (ItemAbilities.SHOVEL_FLATTEN == itemAbility)
+		{
+			return AerialHellBlocks.STELLAR_DIRT_PATH.get().defaultBlockState();
 		}
 		return super.getToolModifiedState(state, context, itemAbility, simulate);
 	}
