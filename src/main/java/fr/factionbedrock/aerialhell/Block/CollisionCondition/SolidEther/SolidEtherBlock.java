4209a543da501964732855783f5183f2fdf799cf
Nicolas Hochart <nicolas@freddom.fr>
2026-09-09 13:48:11 +0200

    [Blocks, Lang] Updated Solid Ether break behavior + Chinese translation

--- src/main/java/fr/factionbedrock/aerialhell/Block/CollisionCondition/SolidEther/SolidEtherBlock.java
+++ src/main/java/fr/factionbedrock/aerialhell/Block/CollisionCondition/SolidEther/SolidEtherBlock.java
@@ -6,13 +6,17 @@ import fr.factionbedrock.aerialhell.Util.EntityHelper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
 
 public class SolidEtherBlock extends CollisionConditionHalfTransparentBlock
 {
@@ -28,11 +32,13 @@ public class SolidEtherBlock extends CollisionConditionHalfTransparentBlock
 		if (!EntityHelper.isFeatheryEntity(entity)) {super.livingEntityInside(state, world, pos, entity);}
 	}
 
-	@Override public void destroy(LevelAccessor world, BlockPos pos, BlockState state)
+	@Override public void playerDestroy(Level level, Player player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack tool)
 	{
-		if (this != AerialHellBlocks.WHITE_SOLID_ETHER)
+		super.playerDestroy(level, player, pos, state, blockEntity, tool);
+
+		if (!EntityHelper.hasEnchantment(player, Enchantments.SILK_TOUCH) && !state.is(AerialHellBlocks.WHITE_SOLID_ETHER))
 		{
-			world.setBlock(pos, AerialHellBlocks.WHITE_SOLID_ETHER.defaultBlockState(), Block.UPDATE_ALL);
+			level.setBlock(pos, AerialHellBlocks.WHITE_SOLID_ETHER.defaultBlockState(), Block.UPDATE_ALL);
 		}
 	}
 
