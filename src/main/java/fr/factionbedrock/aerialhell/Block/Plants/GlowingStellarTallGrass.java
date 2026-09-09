d7e4dcf69447d9fb5e7d96d3b0d7aee992d074b1
Nicolas Hochart <nicolas@freddom.fr>
2026-09-09 02:52:51 +0200

    [Blocks, Entities, Lang, Enchantments] Backported news / fixes

--- src/main/java/fr/factionbedrock/aerialhell/Block/Plants/GlowingStellarTallGrass.java
+++ src/main/java/fr/factionbedrock/aerialhell/Block/Plants/GlowingStellarTallGrass.java
@@ -1,6 +1,5 @@
 package fr.factionbedrock.aerialhell.Block.Plants;
 
-import java.util.function.ToIntFunction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
@@ -9,7 +8,9 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.InsideBlockEffectApplier;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.RedstoneTorchBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
@@ -47,5 +48,10 @@ public class GlowingStellarTallGrass extends AerialHellTallGrassBlock
 
 	@Override protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> state) {state.add(LIT);}
 
-	private static ToIntFunction<BlockState> litBlockEmission(int p_50760_) {return (state) -> {return state.getValue(RedstoneTorchBlock.LIT) ? p_50760_ : 0;};}
+	@Override public boolean isValidBonemealTarget(LevelReader level, BlockPos pos, BlockState state) {return BonemealableBlock.hasSpreadableNeighbourPos(level, pos, state);}
+
+	@Override public void performBonemeal(ServerLevel level, RandomSource random, BlockPos pos, BlockState state)
+	{
+		BonemealableBlock.findSpreadableNeighbourPos(level, pos, state).ifPresent((blockPos) -> level.setBlockAndUpdate(blockPos, this.defaultBlockState()));
+	}
 }
